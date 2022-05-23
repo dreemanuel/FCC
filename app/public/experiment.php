60e1348ce272c53a88e3616bece3ab01aea3bd0e
dreemanuel <drecabron@gmail.com>
2022-05-23 22:43:30 +0800

    first

---
+++ app/public/experiment.php
@@ -0,0 +1,10 @@
+<?php echo 2 + 2; 
+    $myName = 'Andre';
+?>
+
+
+<h1>This page is all about <?php echo $myName ?></h1>
+
+<?php echo 5 * 5 ?>
+
+<h2>A page about <? php echo $myName ?></h2>
---
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,1 @@
+This is our amazing new custom theme
