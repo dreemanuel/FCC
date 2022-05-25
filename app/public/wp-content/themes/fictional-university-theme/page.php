cb27290bde52a82715b7d697255d111b94cf4cf2
dreemanuel <drecabron@gmail.com>
2022-05-26 02:22:51 +0800

    late night updates

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -16,30 +16,42 @@
         </div>
 
     <div class="container container--narrow page-section">
-
         <?php
+            $theParent = wp_get_post_parent_id(get_the_ID()); // if it is a parent page then $theParent would return 0 (false).
+            if ($theParent) {   // if $theParent is TRUE, breadcrumb is active                
+        ?> 
 
-            if ( 2+2 == 4 ) {
-                echo "Two plus two equals four";
-            }
+
+                <!-- BREADCRUMB BAR -->    
+
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                </div> 
+            <?php }
 
         ?>
         
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> 
-            <span class="metabox__main"><?php the_title(); ?></span></p>
-        </div>
 
-        <!--
+
+        <!-- SIDE BAR TO CHILDREN PAGES -->
+
         <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
+            <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
             <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
+                <?php
+                    if ($theParent) {                       // if $theParent =  not 0 (is a child), 
+                        $findChildrenOf = $theParent;       // $findChildrenOf = $theParent.
+                    } else {                                // if $theParent = 0 (is a parent page),
+                        $findChildrenOf = get_the_ID();     // get_the_ID() of the current page.
+                    }
+                    wp_list_pages( array(
+                        'title_li' => NULL,
+                        'child_of' => $findChildrenOf
+                    ));
+                ?>
             </ul>
         </div>
 
-        -->
 
         <div class="generic-content">
             <?php the_content(); ?>
